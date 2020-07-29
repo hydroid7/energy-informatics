068453c146ea6e17d0857e4a05871149a1b48250
Lóránt Meszlényi <meszlenyilorant@gmail.com>
2020-07-29 10:56:57 +0200

    Removing not necessary superclasses.

--- interfaces/interfaces.py
+++ interfaces/interfaces.py
@@ -7,7 +7,7 @@ __author__ = 'Lóránt Meszlényi'
 __email__ = 'meszle01@ads.uni-passau.de'
 
 from abc import ABC, abstractmethod
-from interfaces.informationObjects import GridVoltageReading, TimeIntervalPowerRequirement, GridPowerReading
+from interfaces.informationObjects import TimeIntervalPowerRequirement, GridPowerReading
 from mosaik_api import Simulator
 
 
@@ -23,7 +23,6 @@ class AbstractSimulationObject(ABC, Simulator):
     def create(self, num, model, **model_params):
         pass
 
-
     @abstractmethod
     def step(self, time, inputs):
         """Runs the simulation instance in a simulation step."""
@@ -33,28 +32,24 @@ class AbstractSimulationObject(ABC, Simulator):
     def get_data(self, outputs) -> dict:
         pass
 
+
 class AbstractVPPServer(AbstractSimulationObject):
     """Component that is responsible for the switching of PV systems dependent of the needs of the energy market."""
 
     @abstractmethod
-    def read_power_requirement(self) -> TimeIntervalPowerRequirement:
+    def set_market_energy_requirement(self, requirements: TimeIntervalPowerRequirement):
         pass
 
+    """Calculates the setpoint for the PV controllers."""
     @abstractmethod
-    def calculate_feed_in(self):
-        """Reads the current feed-in power accumulated across all PV controllers."""
-        pass
-
-    @abstractmethod
-    def set_pv_setpoints(self) -> None:
-        """Sets its assigned PV Controllers to the determined value."""
+    def get_pv_setpoints(self):
         pass
 
 
 class AbstractPVController(AbstractSimulationObject):
     """
     Component responsible for switching on/off the PV system. It also adjust the real power output according the local
-    voltage reads.
+    voltage reads and communicates the real power output to the VPPServer.
     """
 
     @abstractmethod
@@ -63,16 +58,15 @@ class AbstractPVController(AbstractSimulationObject):
         pass
 
     @abstractmethod
+    def set_grid_voltage(self):
+        pass
+
+    @abstractmethod
     def set_operation_state(self, feed_in=True) -> None:
         """Sets the controllers state to either "Feed-in" or "Disabled" """
         pass
 
     @abstractmethod
-    def read_grid_voltage(self) -> GridVoltageReading:
-        """Reads the current local grid voltage"""
-        pass
-
-    @abstractmethod
-    def read_real_feed_in(self) -> GridPowerReading:
+    def get_real_feed_in(self) -> GridPowerReading:
         """Reads the current grid feed in"""
         pass
