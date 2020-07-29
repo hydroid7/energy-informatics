775ae005c99aed53e387aa899b5a6117ae9a51e3
Lóránt Meszlényi <meszlenyilorant@gmail.com>
2020-07-29 11:05:12 +0200

    Removes not existing type.

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 def main():
-    from interfaces.interfaces import AbstractLocalMeasurementDevice
-    lmd = AbstractLocalMeasurementDevice() # Expecting error methods not implemented.
 
     from interfaces.informationObjects import GridVoltageReading
     print(GridVoltageReading(voltage=230))
