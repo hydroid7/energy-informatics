3500ac6cf8bfeedb25f5915b32657472e0dd5333
Lóránt Meszlényi <meszlenyilorant@gmail.com>
2020-07-31 12:16:51 +0200

    Setting up basic interfaces.

--- simulators/VPPServer.py
+++ simulators/VPPServer.py
@@ -3,10 +3,16 @@ from typing import List
 from interfaces.informationObjects import TimeIntervalPowerRequirement
 from interfaces.interfaces import AbstractVPPServer
 
+PEAK_PV_POWER = 10000  # in W
+
 
 class VPPServer(AbstractVPPServer):
-    def set_market_energy_requirement(self, requirements: TimeIntervalPowerRequirement):
-        pass
 
-    def get_pv_setpoints(self) -> List:
+    def __init__(self, energy_requirements: TimeIntervalPowerRequirement):
+        self._energy_requirements = energy_requirements
+
+    def set_market_energy_requirement(self, requirements: TimeIntervalPowerRequirement):
+        self._energy_requirements = requirements
+
+    def get_pv_setpoints(self, pv_feed_in: List) -> List:
         pass
